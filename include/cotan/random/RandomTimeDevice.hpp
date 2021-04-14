f82a3d7e2967005f40ad233171f79f3af2b76f91
maxim-palshin <palshin.maxim.alekseevich@gmail.com>
2021-04-14 14:47:36 +0700

    add time random device

---
+++ include/cotan/random/RandomTimeDevice.hpp
@@ -0,0 +1,45 @@
+/*
+   SPDX-FileCopyrightText: 2020 Maxim Palshin
+   <palshin.maxim.alekseevich@gmail.com> SPDX-License-Identifier: BSD 3-Clause
+   "New" or "Revised" License
+*/
+
+_Pragma("once")
+#include <ctime>
+
+namespace cotan {
+
+class RandomTimeDevice{
+
+public:
+
+
+    template<typename T>
+    T time()const {
+        return static_cast<T>(_time());
+    }
+
+    template<>
+    std::time_t time()const{
+        return _time();
+    }
+
+    template<typename T>
+    T generate(){
+        return time<T>();
+    }
+
+
+
+
+private:
+    std::time_t _time()const{
+        return std::time(nullptr);
+    }
+
+
+
+};
+
+
+}
