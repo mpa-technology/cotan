8ba1ccdfd1b82ebe2293efe52c15ee4e5ecc3197
Максим <palshin.maxim.alekseevich@gmail.com>
2021-04-17 23:37:27 +0700

    Delete XorshiftRandEngine.cpp

--- src/random/XorshiftRandEngine.cpp
+++
@@ -1,6 +0,0 @@
-/*
-   SPDX-FileCopyrightText: 2021 Maxim Palshin
-   <palshin.maxim.alekseevich@gmail.com> SPDX-License-Identifier: BSD 3-Clause
-   "New" or "Revised" License
-*/
-#include <cotan/random/XorshiftRandEngine.hpp>
